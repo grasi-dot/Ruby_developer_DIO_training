c7d8f9ad74997809c2604c4a3d6edbe95fd4909e
Grasi Eduarda <grasieduarda775@gmail.com>
2023-08-15 18:16:02 -0300

    regex

---
+++ wpp.rb
@@ -0,0 +1,14 @@
+puts "Validação de número do WhatsApp"
+print "Digite o número do WhatsApp: "
+number = gets.chomp
+
+def validate(number)
+  number = number.gsub(/\D/, '')
+  if number.match?(/(\d{2}\s?)?9\d{4}-?\d{4}$/)
+    return "Número válido: #{number}"
+  else
+    return "Número inválido"
+  end
+end
+
+puts validate(number)
